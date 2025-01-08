d24fca1276363e70643bae790bbe09518c7a8e76
kromanowski00 <krzysztof.romanowski@pimot.lukasiewicz.gov.pl>
2025-01-08 11:03:36 +0100

    feat: add coco format handling

--- scripts/prepare_gan_dataset.py
+++ scripts/prepare_gan_dataset.py
@@ -1,24 +1,26 @@
 import os
 import cv2
 from utils.video_processing import extract_frames
-from utils.annotations import load_yolo_annotations
+from utils.annotations import load_coco_annotations
 
-def prepare_gan_dataset(video_path, annotation_path, output_dir, crop_size=64):
-    frames = extract_frames(video_path)
-    annotations = load_yolo_annotations(annotation_path)
-    
-    os.makedirs(output_dir, exist_ok=True)
-    
-    for frame_number, (class_id, x_center, y_center, width, height) in enumerate(annotations):
-        frame = frames[frame_number]
-        h, w = frame.shape[:2]
-        x1 = int((x_center - width / 2) * w)
-        y1 = int((x_center + width / 2) * w)
-        y1 = int((y_center - height / 2) * h)
-        y2 = int((y_center + height / 2) * h)
-        
-        crop = frame[y1:y2, x1:x2]
-        crop_resized = cv2.resize(crop, (crop_size, crop_size))
-        
-        output_path = os.path.join(output_dir, f'{frame_number}_{class_id}.png')
-        cv2.imwrite(output_path, crop_resized)
+def prepare_gan_dataset(video_path, annotation_path, low_res_output_dir, high_res_output_dir, crop_size=64):
+    frames, filenames = extract_frames(video_path)
+    annotations, id_to_filename = load_coco_annotations(annotation_path)
+   
+    for img_id, anns in annotations.items():
+        filename = id_to_filename[img_id]
+        if filename not in filenames:
+            continue
+           
+        frame_idx = filenames.index(filename)
+        frame = frames[frame_idx]
+       
+        for category_id, bbox in anns:
+            x, y, w, h = [int(c) for c in bbox]
+            crop = frame[y:y+h, x:x+w]
+            crop_resized = cv2.resize(crop, (crop_size, crop_size))
+           
+            low_res_output_path = os.path.join(low_res_output_dir, f'{filename}_{category_id}.png')
+            high_res_output_path = os.path.join(high_res_output_dir, f'{filename}_{category_id}.png')
+            cv2.imwrite(low_res_output_path, crop_resized)
+            cv2.imwrite(high_res_output_path, crop)

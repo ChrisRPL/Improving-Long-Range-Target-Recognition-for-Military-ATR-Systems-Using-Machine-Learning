cd0aa0054af16cb536eb4697fe51d6306291fe40
kromanowski00 <krzysztof.romanowski@pimot.lukasiewicz.gov.pl>
2025-01-08 11:16:45 +0100

    fix: fix gan dataset preparation

--- scripts/prepare_gan_dataset.py
+++ scripts/prepare_gan_dataset.py
@@ -3,8 +3,8 @@ import cv2
 from utils.video_processing import extract_frames
 from utils.annotations import load_coco_annotations
 
-def prepare_gan_dataset(video_path, annotation_path, low_res_output_dir, high_res_output_dir, crop_size=64):
-    frames, filenames = extract_frames(video_path)
+def prepare_gan_dataset(video_path, annotation_path, low_res_output_dir, high_res_output_dir, matching_dict_path, crop_size=32):
+    frames, filenames = extract_frames(video_path, matching_dict_path, "visible")
     annotations, id_to_filename = load_coco_annotations(annotation_path)
    
     for img_id, anns in annotations.items():
@@ -24,3 +24,15 @@ def prepare_gan_dataset(video_path, annotation_path, low_res_output_dir, high_re
             high_res_output_path = os.path.join(high_res_output_dir, f'{filename}_{category_id}.png')
             cv2.imwrite(low_res_output_path, crop_resized)
             cv2.imwrite(high_res_output_path, crop)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Prepare Fusion Dataset")
+    parser.add_argument('--video_frames_path', type=str, required=True, help='Path to the video frames')
+    parser.add_argument('--annotation_path', type=str, required=True, help='Path to coco json annotations')
+    parser.add_argument('--low_res_output_dir', type=str, required=True, help='Path to low res output images')
+    parser.add_argument('--high_res_output_dir', type=str, required=True, help='Path to high res output images')
+    parser.add_argument('--matching_dict_path', type=str, required=True, help='Path to json of rgb-thermal pairs')
+
+    args = parser.parse_args()
+
+    prepare_gan_dataset(args.video_frames_path, args.annotation_path, args.low_res_output_dir, args.high_res_output_dir, args.matching_dict_path)
